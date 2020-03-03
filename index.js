6080eed8be06b0210641e2e2d8c328ca5c16f808
tarunbhargavk18 <tarunbhargavk@gmail.com>
2020-03-03 18:32:23 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.get('/', )
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server Started on port ${PORT}`));
