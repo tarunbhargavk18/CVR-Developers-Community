f791ced46fc0b7d452cc03b29d6b2b4127c2e034
tarunbhargavk18 <tarunbhargavk@gmail.com>
2020-03-25 21:27:43 +0530

    Routes defined

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const app = express();
 //Connect Database
 connectDB();
 
+//Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/feed', require('./routes/api/feed'));
+
+
 
 const PORT = process.env.PORT || 4000;
 
